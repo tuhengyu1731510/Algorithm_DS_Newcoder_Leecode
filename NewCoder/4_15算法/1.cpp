8eb59d73b497c4f03b5188ea0a1da0baadb94e87
宇恒 赵 <2743096775@qq.com>
2024-04-23 20:58:36 +0800

    添加项目文件。

---
+++ NewCoder/4_15算法/1.cpp
@@ -0,0 +1,22 @@
+////#define _CRT_SECURE_NO_WARNINGS 1
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//using namespace std;
+//int num2(int L, int R) {
+//	int cnt = 0;
+//	for (int i = L; i <= R; i++) {
+//		int tmp = i;
+//		while (tmp) {
+//			if (tmp % 10 == 2)
+//				cnt++;
+//			tmp /= 10;
+//		}
+//	}
+//	return cnt;
+//} 
+//int main() {
+//	int L, R;
+//	scanf("%d %d", &L, &R);
+//	printf("%d", num2(L, R));
+//	return 0;
+//}
