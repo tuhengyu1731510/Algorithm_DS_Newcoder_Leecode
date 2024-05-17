888dede993455122b3662b44fc4d5800f52ff29c
thy <129579263+tuhengyu1731510@users.noreply.github.com>
2024-05-18 00:37:21 +0800

    SqList_Definition

---
+++ Awd_VSCode/WangDaoDS.cpp
@@ -0,0 +1,48 @@
+﻿//#include<stdlib.h>
+//#define InitSize 10
+//typedef struct {
+//	int* data;
+//	int MaxSize;
+//	int length;
+//}SeqList;
+//void InitList(SeqList& L) {
+//	L.data = (int*)malloc(InitSize*sizeof(int));
+//	L.length = 0;
+//	L.MaxSize = InitSize;
+//}
+//void IncreaseSize(SeqList &L, int len) {
+//	int* p = L.data;
+//	L.data= (int*)malloc((L.MaxSize+len)*sizeof(int));
+//	for (int i = 0; i < L.length; i++) {
+//		L.data[i] = p[i];
+//	}
+//	L.MaxSize = L.MaxSize + len;
+//	free(p);
+//}
+//int main() {
+//	SeqList L;
+//	InitList(L);
+//	IncreaseSize(L, 5);
+//}
+#include<bits/stdc++.h>
+#define MaxSize 10
+typedef struct {
+	int data[MaxSize];
+	int length;
+}SqList;
+void InitList(SqList &L) {//创建List
+	L.length = 0;
+}
+
+void ListInsert(SqList& L, int i, int e) {
+	for (int j = L.length; j >= i; j--)
+		L.data[j] = L.data[j - 1];
+	L.data[i - 1] = e;
+	L.length++;
+}
+int main() {
+	SqList L;
+	InitList(L);
+	ListInsert(L, 3, 3);
+	return 0;
+}
