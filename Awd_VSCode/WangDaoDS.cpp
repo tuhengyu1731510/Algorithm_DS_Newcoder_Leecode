1ffe89fe09661f68a11b8bce800448dc7ed0c2fa
thy <129579263+tuhengyu1731510@users.noreply.github.com>
2024-05-19 23:15:44 +0800

    王道DS

--- Awd_VSCode/WangDaoDS.cpp
+++ Awd_VSCode/WangDaoDS.cpp
@@ -24,25 +24,31 @@
 //	InitList(L);
 //	IncreaseSize(L, 5);
 //}
-#include<bits/stdc++.h>
-#define MaxSize 10
-typedef struct {
-	int data[MaxSize];
-	int length;
-}SqList;
-void InitList(SqList &L) {//创建List
-	L.length = 0;
-}
-
-void ListInsert(SqList& L, int i, int e) {
-	for (int j = L.length; j >= i; j--)
-		L.data[j] = L.data[j - 1];
-	L.data[i - 1] = e;
-	L.length++;
-}
-int main() {
-	SqList L;
-	InitList(L);
-	ListInsert(L, 3, 3);
-	return 0;
-}
+//#include<bits/stdc++.h>
+//#define MaxSize 10
+//typedef struct {
+//	int data[MaxSize];
+//	int length;
+//}SqList;
+//void InitList(SqList &L) {//创建List
+//	L.length = 0;
+//}
+//
+//void ListInsert(SqList& L, int i, int e) {
+//	for (int j = L.length; j >= i; j--)
+//		L.data[j] = L.data[j - 1];
+//	L.data[i - 1] = e;
+//	L.length++;
+//}
+//int main() {
+//	SqList L;
+//	InitList(L);
+//	ListInsert(L, 3, 3);
+//	return 0;
+//} 
+#include<stdlib.h>
+struct LNode {
+	int data;
+	struct LNode* next;
+};
+struct LNode* p = (struct LNode*)malloc(sizeof(struct LNode));
